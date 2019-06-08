4186cc82c9c9b2c70f4c7fa05baa736a4e79827b
Valentin <vnardoni@live.com.ar>
2019-06-08 11:46:04 -0300

    tablas

---
+++ Tablas/0007_create_table_rolesxpermisos.sql
@@ -0,0 +1,9 @@
+USE Mantenimiento;
+
+CREATE TABLE rolesxpermisos (
+    idRol INTEGER;
+    idPermiso INTEGER;
+    PRIMARY KEY (idRol,idPermiso),
+    FOREIGN KEY (idRol) REFERENCES roles(idRol),
+    FOREIGN KEY (idPermiso) REFERENCES permisos(idPermiso)
+)
