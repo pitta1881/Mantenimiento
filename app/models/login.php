8e09fcc7f2d1dc3e3f12ea67529e098098f9a3b8
Patricio <pitta1881@gmail.com>
2019-06-09 22:33:17 -0300

    asd

--- app/models/login.php
+++ app/models/login.php
@@ -7,15 +7,7 @@ use App\Core\Model;
 class login extends Model{
     protected $table = 'usuarios';
 
-
     public function buscarUsuario($user, $password){
         return $this->db->validarLogin($this->table,$user, $password);
     }
-
-    public function getUsuario($nombreUsuario){
-        return $this->db->buscarUsuario($tthis->table,$nombreUsuario);
-    }
-    
-
-    
 }
