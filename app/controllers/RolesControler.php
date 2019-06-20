873ae0f1ed3e41e2d373cb89b4fc34257f70bda2
Iván <costaivan34@gmail.com>
2019-06-20 00:47:03 -0300

    vista roles sin terminar, cambio de estilo de pagina no encontrada

--- app/controllers/RolesControler.php
+++ app/controllers/RolesControler.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controllers;
+/*namespace App\Controllers;
 
 use App\Core\Controller;
 use App\Models\Pedido;
@@ -12,14 +12,14 @@ class PedidoController extends Controller
         $this->model = new Pedido();
     }
 
-    /*Show all pedidos*/
+    /*Show all pedidos
     public function index()
     {
         $todosPedidos = $this->model->get();
         return view('verTodosPedidos', compact('todosPedidos'));
     }
 
-    /*muestra un solo pedido especifico ingresado por GET*/
+    /*muestra un solo pedido especifico ingresado por GET
     public function ficha()
     {
         $unPedido = $this->model->getByIdPedido($_GET['id']);
@@ -45,7 +45,7 @@ class PedidoController extends Controller
     }
 
     public function validar(){
-       /*
+      
        //ACA SE PODRIA VALIDAR ALGO MAS ADELANTE
        //
        $estaBien = $this->model->validarDatos($_POST);
@@ -55,7 +55,7 @@ class PedidoController extends Controller
        } else {
            echo "<h2>Algo salio Mal</h2>";
        }       
-       */
+       
       $arrayPedido = $this->save();
       return view('verPedidoCreado',compact('arrayPedido'));
     }
@@ -88,4 +88,105 @@ class PedidoController extends Controller
         return view('modificarPedido',compact('arrayDatos'));
     }
 
+}*/
+<?php
+
+namespace App\Controllers;
+
+use App\Core\Controller;
+use App\Models\Roles;
+use App\Models\Permisos;
+
+class PedidoController extends Controller{
+
+    public function __construct(){
+        $this->model = new Roles();
+        session_start();
+    }
+
+    /*Show all pedidos*/
+    public function index(){
+        if(isset($_GET["page"])){
+            $pagina=$_GET["page"];
+        }else{
+            $pagina=1;
+        }
+        $todosRoles = $this->model->getPaginacion($pagina); 
+        $datos['todosRoles'] = $todosRoles;
+        $totalPaginas=$this->model->getsize();
+        $datos["totalPaginas"] =   $totalPaginas;
+        $datos["permisos"] = $this->model->getPermisos();
+
+        $datos["userLogueado"] = $_SESSION['user'];
+        return view('/rol/administracionRol', compact('datos'));
+    }
+
+    /*muestra un solo pedido especifico ingresado por GET*/
+    public function ficha(){
+        $miPedido = $this->model->getByIdPedido($_GET['id']);
+        $datos["miROL"] = $miPedido;  
+        $datos["permisos"] = $this->model->getPermisos();
+        $datos["userLogueado"] = $_SESSION['user'];
+        return view('/pedidos/pedidoVerFicha', compact('datos'));
+    }
+
+    public function guardar(){
+        $pedido = [
+            'fechaInicio' => $_POST['fechaInicio'],
+            'estado' => $_POST['estado'],
+            'descripcion' => $_POST['descripcion'],
+            'prioridad' => $_POST['prioridad'],
+            'nombreUsuario' => $_POST['nombreUsuario']
+        ];
+      $this->model->insert($pedido);
+      if(!empty($_POST['idEvento'])){
+        var_dump($_POST['idEvento']);  
+        $this->model->eliminarEvento($_POST['idEvento']);
+      }
+      $datos['arrayPedido'] = $pedido;
+      $datos["userLogueado"] = $_SESSION['user'];
+      $idNuevoPedido = $this->model->getIdUltimoPedido();
+      redirect("fichaPedido?id=".$idNuevoPedido);
+    }
+
+    public function modificarPedidoSeleccionado(){
+        $unPedido = $this->model->getByIdPedido($_GET['id']);
+        $unPedido['fechaInicio'] = date("Y-m-d",strtotime(str_replace('/', '-', $unPedido['fechaInicio'])));
+        $datos["sectores"] = $this->model->getSectores();
+        $datos["prioridades"] = $this->model->getPrioridades();
+        $datos["estados"] = $this->model->getEstados();
+        $datos["miPedido"] = $unPedido;
+        $datos["userLogueado"] = $_SESSION['user'];
+        return view('/pedidos/pedidoModificar',compact('datos'));
+    }
+       
+
+    public function modificar(){
+        $idPedido = $_POST['id'];
+        $idSector = $this->model->getIdSectorPorNombre($_POST['sector']);
+        $datos['idSector']= $idSector;
+        $arrayPedido = [
+            'fechaInicio' => $_POST['fechaInicio'],
+            'estado' => $_POST['estado'],
+            'descripcion' => $_POST['descripcion'],
+            'idSector' => $idSector,
+            'prioridad' => $_POST['prioridad'],
+        ];
+        $this->model->updatePedido($arrayPedido,$idPedido);
+        redirect("fichaPedido?id=".$idPedido);
+    }
+    
+    public function buscarPor(){
+        $filter = $_POST['filtro'];
+        $value = $_POST['textBusqueda'];
+        $datos['todosPedidos'] = $this->model->getAllbyFilter($filter,$value);
+        $datos['userLogueado'] = $_SESSION['user'];           
+        return view('/pedidos/pedidosVerTodos', compact('datos'));         
+     }
+
+     public function finalizar(){
+         $this->model->updateEstadoPedido($_POST['id'],'Finalizado');
+         redirect("fichaPedido?id=".$_POST['id']);
+     }
 }
+
