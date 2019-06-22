4369fe55988f66fa7a1444f081aef21004ccf8d1
Iván <costaivan34@gmail.com>
2019-06-22 13:30:34 -0300

    update permisos

--- app/models/Permisos.php
+++ app/models/Permisos.php
@@ -6,51 +6,7 @@ use App\Core\Model;
 
 class Permisos extends Model{
     protected $table = 'permisos';
-    protected $size_pagina=8;
-
-    
-    public function get(){
-        $eventos = $this->db->selectAll($this->table);      
-        $todosEventos = json_decode(json_encode($eventos), True);
-        foreach ($todosEventos as $indice => $datos) {
-            foreach ($datos as $key => $value) {
-                if ($key == 'fechaInicio') {
-                    $todosEventos[$indice]['fechaInicio'] = date("d/m/Y", strtotime($todosEventos[$indice]['fechaInicio']));
-                }
-                if ($key == 'fechaFin') {
-                    $todosEventos[$indice]['fechaFin'] = date("d/m/Y", strtotime($todosEventos[$indice]['fechaFin']));
-                }
-            }
-        }          
-        return $todosEventos;
-    }
-
-   public function buscarEvento($nombreEvento){
-       //comparo si existe el evento
-       return $this->db->comparaEventos($this->table,$nombreEvento);
-    }
-
-    public function insert(array $datos)
-    {
-        
-        $this->db->insert($this->table, $datos);
-    }
-
-    public function getByIdEvento($idEvento){
-        $evento = $this->db->selectEventoById($this->table,$idEvento);
-        $miEvento = json_decode(json_encode($evento), True);  
-        return $miEvento[0];
-    }
-
-    public function update(array $datos,$idEvento)
-    {
-        $this->db->updateEvento($this->table, $datos,$idEvento);
-    }
-
-    public function delete($nEvento){
-        
-        $this->db->deleteEvento($this->table,$nEvento);
-     }  
+    protected $size_pagina=10;
     
      public function getSize(){
         $num_filas= $this->db->getSize($this->table);
@@ -63,10 +19,12 @@ class Permisos extends Model{
         $empezar_desde=($pagina-1)*$this->size_pagina;
         $num_filas= $this->getSize();
         $total_paginas= ceil($num_filas/$this->size_pagina);
-        $eventos = $this->db->getAllLimit($this->table,$empezar_desde,$this->size_pagina);    
-        $todosEventos = json_decode(json_encode($eventos), True);  
-        return $todosEventos;
+        $especializaciones = $this->db->getAllLimit($this->table,$empezar_desde,$this->size_pagina);
+        $todasEspecializaciones = json_decode(json_encode($especializaciones), True);
+       
+        return $todasEspecializaciones;
     }
+    
 
 
 }
